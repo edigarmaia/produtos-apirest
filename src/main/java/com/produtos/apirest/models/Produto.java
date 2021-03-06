91c78c38372f93987d978d5e5cc4e44f2d77c239
Edigar Maia <edigarmaia@gmail.com>
2021-03-05 22:46:06 -0300

    ção da classe ProdutoResource Controller

--- src/main/java/com/produtos/apirest/models/Produto.java
+++ src/main/java/com/produtos/apirest/models/Produto.java
@@ -20,7 +20,7 @@ public class Produto implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private long id;
+	private Long id;
 	@NotNull
 	private String nome;
 	@NotNull
